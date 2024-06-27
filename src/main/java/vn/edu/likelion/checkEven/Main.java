db34d71d65fb292d9654e3bb449bfc8fb02d7b41
Khang0202 <nguyenmanhkhang2002@gmail.com>
2024-06-27 14:31:10 +0700

    exam 2 3 21/06/2024

---
+++ src/main/java/vn/edu/likelion/checkEven/Main.java
@@ -0,0 +1,51 @@
+package vn.edu.likelion.checkEven;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 256789113;
+        int b = 2468268;
+        show(a);
+        System.out.println("---------------------------------------");
+        show(b);
+    }
+
+    public static void show(int n){
+        System.out.println("Input an interger: " + n);
+        System.out.println("Check whether every digit of the said integer is even or not!");
+        System.out.println(checkArray(n));
+    }
+    public static boolean checkEven(int n) {
+        if (n % 2 == 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static boolean checkArray(int n) {
+        LinkedList<Integer> list = toList(n);
+        boolean flag = false;
+        for (int i = 0; i < list.size(); i++) {
+            if (checkEven(list.get(i))) {
+                flag = true;
+            }else {
+                flag = false;
+                return flag;
+            }
+        }
+        return flag;
+    }
+
+    public static LinkedList<Integer> toList(int n) {
+        LinkedList<Integer> list = new LinkedList<>();
+        do{
+            list.add(n%10);
+            n=n/10;
+        }while (n>10);
+        list.add(n);
+        return list;
+    }
+}
--- src/main/java/vn/edu/likelion/moveall0toend/Main.java
+++ src/main/java/vn/edu/likelion/moveall0toend/Main.java
@@ -3,20 +3,26 @@ package vn.edu.likelion.moveall0toend;
 public class Main {
     public static void main(String[] args) {
         int[] arr = { 0,1,5,6,0,3,5,9,0,8,7,9,0 };
-        int[] res = moveAll0ToEnd(arr);
-        for (int i = 0; i < res.length; i++) {
-            System.out.print(res[i]+", ");
+        System.out.println("Input:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+"\t");
+        }
+        moveAll0ToEnd(arr);
+        System.out.println();
+        System.out.println("Output:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+"\t");
         }
     }
 
-    public static int[] moveAll0ToEnd(int[] arr) {
+    public static void moveAll0ToEnd(int[] arr) {
         for (int i = 0; i < arr.length-1; i++) {
             if (arr[i] == 0) {
                 for (int j = i+1; j < arr.length; j++) {
                     arr[j-1] = arr[j];
                 }
+                arr[arr.length-1] = 0;
             }
         }
-        return arr;
     }
 }
